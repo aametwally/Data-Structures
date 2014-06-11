4e7df4882c00badf0dd8a52055942f9adf90f6d7
Ahmed Abdelhadi <hady.bio@gmail.com>
2014-06-11 08:19:45 +0300

    Update Stack_by_Array.c

--- Stack_by_Array.c
+++ Stack_by_Array.c
@@ -1,15 +1,15 @@
 ///**********************************************************************///
-///******     	   			   Stack using Array	      	 	   ******///
-///******  		  Title: Push, Pop, and Traverse Functions		   ******///
-///******                  			                 		 	   ******///
-///******  		     	 [DataStructure Course, 2014]  			   ******///
-///******		   Biomedical Engineering, Cairo University	 	   ******///
+///******     	   	       Stack using Array	      	   ******///
+///******  	  Title: Push, Pop, and Traverse Functions         ******///
+///******                  			                   ******///
+///******  	        [DataStructure Course, 2014]  		   ******///
+///******	  Biomedical Engineering, Cairo University	   ******///
 ///**********************************************************************///
-///*                                                             	    *///
-///*   		  	 	Copyright by Ahmed Abdelhadi (C) 2014  	   	  	    *///
-///*                                                               		*///
-///*              	 Please report bugs & suggestions to              	*///
-///*                   <ahmed.abdelhady@eng.cu.edu.eg>               	*///
+///*                                                             	*///
+///*   		   Copyright by Ahmed Abdelhadi (C) 2014  	   	*///
+///*                                                               	*///
+///*                Please report bugs & suggestions to              	*///
+///*                  <ahmed.abdelhady@eng.cu.edu.eg>               	*///
 ///**********************************************************************///
 
 #include <stdio.h>
